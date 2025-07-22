6c3da673f2eac637632f1538cb07957e74ab82cd
Wessam100 <125602282+Wessam100@users.noreply.github.com>
2025-07-22 21:27:50 +0300

    Increased video resolution

--- test-webpage/index.js
+++ test-webpage/index.js
@@ -14,7 +14,21 @@ let minVy =  Infinity, maxVy = -Infinity;
 const collectedData = []; // This is where we will save gaze data
 let currentCalibrationTarget = null; // Current red dot position
 let isCollecting = false; // Whether we are currently collecting data
-
+/////////////////////////////////////////////////////////////// remove //////////////////////////////////////////
+const eyeCanvas = document.createElement('canvas');
+eyeCanvas.width = 200;  // adjust for zoom level
+eyeCanvas.height = 100;
+eyeCanvas.style.border = "1px solid #ccc";
+eyeCanvas.style.position = "absolute";
+eyeCanvas.style.bottom = "10px";
+eyeCanvas.style.left = "10px";
+document.body.appendChild(eyeCanvas);
+const eyeCtx = eyeCanvas.getContext('2d');
+//////////////////////////////////////////////////////////////remove /////////////////////////////////////////////////
+navigator.mediaDevices.getUserMedia({ // increase the video resolution to improve gaze tracking accuracy
+  video: { width: 1280, height: 720 },
+  facingMode: "user" // Use the front camera
+})
 let heatCanvas, heatCtx;
 function createHeatMapLayer() { // Create a canvas for the heatmap layer for detecting patterns in gaze movements
   heatCanvas = document.createElement('canvas');
@@ -236,8 +250,34 @@ async function continueDetection(video, detector,canvas,cursor,gazeModel) {
 
         const rightEyeInnerCorner = keypoints[362]; 
         const rightEyeOuterCorner = keypoints[263];
+        // Crop and zoom both eyes
+        //////////////////////////////////////////remove ///////////////////////////////////////////////////////////////////
+        function getEyeBox(inner, outer, margin = 10) {
+            const x = Math.min(inner.x, outer.x) - margin;
+            const y = Math.min(inner.y, outer.y) - margin;
+            const width = Math.abs(inner.x - outer.x) + margin * 2;
+            const height = width / 2; // approx shape of eye
+            return { x, y, width, height };
+        }
 
+        const leftBox = getEyeBox(keypoints[133], keypoints[33]);   // Left eye corners
+        const rightBox = getEyeBox(keypoints[362], keypoints[263]); // Right eye corners
 
+        eyeCtx.clearRect(0, 0, eyeCanvas.width, eyeCanvas.height);
+
+        eyeCtx.drawImage(
+            video,
+            leftBox.x, leftBox.y, leftBox.width, leftBox.height,
+            0, 0, eyeCanvas.width / 2, eyeCanvas.height
+        );
+
+        eyeCtx.drawImage(
+            video,
+            rightBox.x, rightBox.y, rightBox.width, rightBox.height,
+            eyeCanvas.width / 2, 0, eyeCanvas.width / 2, eyeCanvas.height
+        );
+
+    /////////////////////////////////////////////////////////remove ///////////////////////////////////////////////////////
 
         // Mirror drawing to match video
         ctx.save();
